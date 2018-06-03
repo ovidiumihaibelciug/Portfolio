7add02a946027e35c7f2240780b2cc5963431d86
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2018-06-04 01:23:34 +0300

    Particles changed

--- src/containers/views/Home.jsx
+++ src/containers/views/Home.jsx
@@ -28,14 +28,63 @@ class Home extends Component {
                         <Particles
                             params={{
                                 particles: {
-                                    line_linked: {
-                                        shadow: {
+                                    number: {
+                                        value: 20,
+                                        density: {
                                             enable: true,
-                                            color: "#004567",
-                                            blur: 1
+                                            value_area: 1500
+                                        }
+                                    },
+                                    size: {
+                                        value: 1.1
+                                    },
+                                    line_linked: {
+                                        enable: true,
+                                        distance: 150,
+                                        color: "#ffffff",
+                                        opacity: 0.4,
+                                        width: 1
+                                    },
+                                    "shape": {
+                                        "type": "star",
+                                        "stroke": {
+                                            "width": 0,
+                                            "color": "#000000"
+                                        },
+                                        polygon: {
+                                            nb_sides: 7
                                         }
                                     }
-                                }
+                                },
+                                "image": { "src": LOGO, "width": 100, "height": 100 },
+                                "interactivity": {
+                                    "detect_on": "canvas",
+                                    "events": {
+                                        "onhover": {
+                                            "enable": true,
+                                            "mode": "repulse"
+                                        },
+                                        "onclick": {
+                                            "enable": true,
+                                            "mode": "repulse"
+                                        },
+                                        "resize": true
+                                    },
+                                    "modes": {
+                                        "grab": {
+                                            "distance": 400,
+                                            "line_linked": {
+                                                "opacity": 1
+                                            }
+                                        },
+                                        "repulse": {
+                                            "distance": 200,
+                                            "duration": 0.4
+                                        }
+                                    },
+                                },
+                                "retina_detect": true
+
                             }}
                         />
                     </div>
