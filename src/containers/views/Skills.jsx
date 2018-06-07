71ad0d7e02e24689e09f40d357b9156372b753f3
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2018-06-07 21:47:35 +0300

    Skill progress bars

--- src/containers/views/Skills.jsx
+++ src/containers/views/Skills.jsx
@@ -2,6 +2,9 @@ import React, { Component, Fragment } from 'react';
 
 import Navbar from '../../components/navbar/Navbar';
 
+import CircularProgressbar from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 export default class Skills extends Component {
 
     componentDidMount() {
@@ -14,6 +17,7 @@ export default class Skills extends Component {
     }
 
     render() {
+        const yourPercentage = 75;
         return (
             <Fragment>
                 <div className="App Skills">
@@ -25,13 +29,60 @@ export default class Skills extends Component {
                         <div className="content">
                             <span data-hide="10" className="tag tag-render tag-1">{"render() {"}</span>
                             <span data-hide="4000" className="tag tag-fragment-start tag-2">{"<Fragment>"}</span>
+                            <div className="container">
+                                <div className="flex-row">
+                                    <div className="left-side">
 
-                            <span data-hide="8000" className="tag tag-fragment-end tag-3">{"</Fragment>"}</span>
-                            <span data-hide="12000" className="tag tag-render-end tag-4">{"}"}</span>
+                                        <div class="set-size charts-container">
+                                            <div class="pie-wrapper progress-95 style-2">
+                                                <span class="label">95<span class="smaller">%</span></span>
+                                                <div class="pie">
+                                                    <div class="left-side half-circle"></div>
+                                                    <div class="right-side half-circle"></div>
+                                                </div>
+                                                <div class="shadow"></div>
+                                            </div>
+                                        </div>
+                                        <div className="progress-section-text">HTML&CSS</div>
+
+                                    </div>
+                                    <div className="right-side">
+                                        <div className="skill">
+                                            <div className="skill-label">
+                                                <div className="skill-name">ABCD</div>
+                                                <div className="skill-procent">95%</div>
+                                            </div>
+                                            <div className="skill-bar">
+                                                <div className="skill-progress"></div>
+                                            </div>
+                                        </div>
+                                        <div className="skill">
+                                            <div className="skill-label">
+                                                <div className="skill-name">ABCD</div>
+                                                <div className="skill-procent">75%</div>
+                                            </div>
+                                            <div className="skill-bar">
+                                                <div className="skill-progress" style={{ width: "75%" }}></div>
+                                            </div>
+                                        </div>
+                                        <div className="skill">
+                                            <div className="skill-label">
+                                                <div className="skill-name">ABCD</div>
+                                                <div className="skill-procent">95%</div>
+                                            </div>
+                                            <div className="skill-bar">
+                                                <div className="skill-progress"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <span data-hide="8000" className="tag tag-fragment-end tag-3">{"</Fragment>"}</span>
+                        <span data-hide="12000" className="tag tag-render-end tag-4">{"}"}</span>
                     </div>
                 </div>
-            </Fragment>
+            </Fragment >
         )
     }
 }
