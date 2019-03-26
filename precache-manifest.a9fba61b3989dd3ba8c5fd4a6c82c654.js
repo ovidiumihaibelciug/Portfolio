b5f9ad1c3be723c1789987b7fac882854ffb9878
ovidiumihaibelciug <ovidiumihaibelciug@gmail.com>
2019-03-26 19:34:35 +0200

    Updates

---
+++ precache-manifest.a9fba61b3989dd3ba8c5fd4a6c82c654.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b59badc3df50034e5326cf4ff10e5e58",
+    "url": "/Portfolio/static/media/div_pattern.b59badc3.jpg"
+  },
+  {
+    "revision": "f82b84cf8d02d5535751",
+    "url": "/Portfolio/static/css/main.a7c27558.chunk.css"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "73108bdf2936e4cb90a5",
+    "url": "/Portfolio/static/js/2.809e8ff3.chunk.js"
+  },
+  {
+    "revision": "7c148617027800771a54f3b8d1c2ebe6",
+    "url": "/Portfolio/static/media/LOGO.7c148617.svg"
+  },
+  {
+    "revision": "3953f436895fbc204e09438f61f5fb6d",
+    "url": "/Portfolio/static/media/LOGO.3953f436.png"
+  },
+  {
+    "revision": "39c12d836806c6bdaa0748369706935e",
+    "url": "/Portfolio/static/media/education.39c12d83.jpg"
+  },
+  {
+    "revision": "f82b84cf8d02d5535751",
+    "url": "/Portfolio/static/js/main.3f0141ac.chunk.js"
+  },
+  {
+    "revision": "92d7573f021d96f8494791b95e8ca080",
+    "url": "/Portfolio/static/media/about.92d7573f.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "73108bdf2936e4cb90a5",
+    "url": "/Portfolio/static/css/2.1bea2de1.chunk.css"
+  },
+  {
+    "revision": "79571032401566f7927093a2ee02efb6",
+    "url": "/Portfolio/index.html"
+  }
+];
